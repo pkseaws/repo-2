f786724bcd4f3808a474eeb53e0ec616eebb688a
pkseaws <164235580+pkseaws@users.noreply.github.com>
2025-08-05 15:32:27 +0530

    Create test.java

---
+++ java/test.java
@@ -0,0 +1,15 @@
+public class Task2 {
+
+    // Main method: Entry point of the program
+    public static void main(String[] args) {
+        System.out.println("Task - 2");
+
+        // Call a method
+        greetUser("Pratyush");
+    }
+
+    // Method that takes a name and prints a greeting
+    public static void greetUser(String name) {
+        System.out.println("Hello, " + name + "! Welcome to GitHub Actions.");
+    }
+}
